c8708efacb924b5322205343e9aee3de8f07c641
chiranjit <chiranjitengineer@gmail.com>
2020-09-05 23:17:52 +0530

    init commit....

---
+++ EasyCompSolutionOfficial/EasyCompSolutionOfficial/Scripts/Script.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../node_modules/angular/angular.min.js" />
+/// <reference path="../node_modules/angular-material/angular-material.min.js" />
+
+//'ngMaterial'
+angular.module('buttonsDemoBasic', ['ngMaterial'])
+.controller('AppCtrl', function ($scope) {
+    $scope.title1 = 'Button';
+    $scope.title4 = 'Warn';
+    $scope.isDisabled = true;
+    $scope.googleUrl = 'http://google.com';
+    
+   
+});
