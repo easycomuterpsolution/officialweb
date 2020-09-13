7717a52fca2e1e46e58e842003b98afef166546b
Chiranjit <chiranjitengineer@gmail.com>
2020-09-13 23:35:27 +0530

    insert project page

--- EasyCompSolutionOfficial/EasyCompSolutionOfficial/Controllers/ProjectsController.cs
+++ EasyCompSolutionOfficial/EasyCompSolutionOfficial/Controllers/ProjectsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EasyCompSolutionOfficial.Models;
+
 
 namespace EasyCompSolutionOfficial.Controllers
 {
@@ -13,8 +15,30 @@ namespace EasyCompSolutionOfficial.Controllers
 
         public ActionResult Index()
         {
+            YoutubeLinksLayer layers = new YoutubeLinksLayer();
+
+            List<YoutubeLink> list = layers.GetAllLinks();
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollection)
+        {
+            YoutubeLinksLayer layers = new YoutubeLinksLayer();
+            YoutubeLink ll = new YoutubeLink();
+
+            ll.Link = formCollection["Link"];
+            ll.Title = formCollection["Title"];
+
+            bool v = layers.AddNewLink(ll);
+            RedirectToAction("Index",);
+            return View();
+        }
     }
 }
---
+++ EasyCompSolutionOfficial/EasyCompSolutionOfficial/Models/YoutubeLinks.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Configuration;
+
+namespace EasyCompSolutionOfficial.Models
+{
+    public class YoutubeLinksLayer
+    {
+        public YoutubeLinksLayer()
+        {
+
+        }
+        public List<YoutubeLink> GetAllLinks()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBECSPROJConnectionString"].ConnectionString;
+
+            List<YoutubeLink> links = new List<YoutubeLink>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllYoutubeLinks", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    YoutubeLink link = new YoutubeLink();
+                    link.Id = Convert.ToInt32(rdr["Id"]);
+                    link.Link = rdr["YoutubeLink"].ToString();
+                    link.Title = rdr["Title"].ToString();
+                    links.Add(link);
+                }
+            }
+
+            return links;
+        }
+        public bool AddNewLink(YoutubeLink link)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBECSPROJConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spInsertYoutubeLinks", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //SqlDataReader rdr = cmd.ExecuteReader();
+
+                SqlParameter paramName = new SqlParameter();
+                paramName.ParameterName = "@Link";
+                paramName.Value = link.Link;
+                cmd.Parameters.Add(paramName);
+
+                 paramName = new SqlParameter();
+                paramName.ParameterName = "@Title";
+                paramName.Value = link.Title;
+                cmd.Parameters.Add(paramName);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            return true;
+        }
+    }
+
+    public class YoutubeLink
+    {
+        public int Id { get; set; }
+        public string Link { get; set; }
+        public string Title { get; set; }
+    }
+}
